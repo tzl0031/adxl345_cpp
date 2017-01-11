6204dae72438ed8a552a3fecc32511a12b7c9d48
tzl0031 <tzl0031@tigermail.auburn.edu>
2017-01-11 15:07:54 -0600

    add time function to test how fast it can reach

--- adxl.cpp
+++ adxl.cpp
@@ -2,7 +2,9 @@
 #include "I2Cdev.h"
 #include <bcm2835.h>
 #include <stdio.h>
+#include <sys/time.h>
 
+struct timeval start, end;
 int main(int argc, char **argv) {
   printf("ADXL345 3-axis acceleromter example program\n");
   I2Cdev::initialize();
@@ -16,11 +18,14 @@ int main(int argc, char **argv) {
   accel.initialize();
   int16_t ax, ay, az;
   while (true) {
+    gettimeofday(&start, NULL);
     accel.getAcceleration(&ax, &ay, &az);
     printf("  x_raw:  0x%04X       y_raw:  0x%04X      z_raw:  0x%04X\r", ax,
            ay, az);
     fflush(stdout);
-    bcm2835_delay(200);
+    gettimeofday(&end, NULL);
+    diff = (end.tv_sec - start.tv_sec)*1000 + (end.tv_usec - start.tv_usec) /1000;
+    cout << "the difference is " << diff << "ms" << endl;  
   }
   return 1;
 }
